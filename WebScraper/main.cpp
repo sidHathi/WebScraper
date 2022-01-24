59464a5963cfc681302c78b8c41850187eb18d9c
Sid Hathi <sidhathi20@gmail.com>
2022-01-24 01:35:56 +0100

    Initial Commit

---
+++ WebScraper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WebScraper
+//
+//  Created by Siddharth Hathi on 1/24/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
